fec7726e79e59d17936a2047663e32ceb29e001f
Andres Munoz <andrefe91@gmail.com>
2024-06-16 17:08:46 -0500

    Development code end result

--- webpack.common.js
+++ webpack.common.js
@@ -2,26 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    app: './src/index.js',
   },
-  devtool: 'eval',
-  devServer: {
-    static: './dist',
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Production',
     }),
   ],
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
